9f8c009d7fedcfd6bd90e28f0b20d863240bd8ca
hemanth-h15 <hemanth.harish1999@gmail.com>
2025-06-06 15:23:18 +0530

    Add files via upload

---
+++ AverageMarks.java
@@ -0,0 +1,20 @@
+package Hellojava;
+
+public class AverageMarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Maths = 36;
+		int Science = 41;
+		int English = 80;
+		
+		float TotalScore = (Maths + Science + English);
+		
+		System.out.print("Total Score =  "  + (Maths + Science + English));
+		System.out.print("Avarage is "  + (TotalScore / 3));
+		
+
+	}
+
+}
